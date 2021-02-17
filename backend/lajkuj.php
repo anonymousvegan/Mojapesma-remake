a62fc5890c4ff0a94535a4801ccb0111627d6230
anonymousvegan <majjunjulavgust@gmail.com>
2021-02-17 02:28:00 +0100

    Like now works

--- backend/lajkuj.php
+++ backend/lajkuj.php
@@ -1,87 +1,66 @@
 <?php
 session_start();
-if(isset($_SESSION["ime"])){
-    require "pozadinske/vezasabazom.php";
-    $id = $_POST["id"];
-    $profil= $_POST["profil"];
-    $ime = $_SESSION["ime"]; 
-    if($profil==$ime){
-        $sql = "SELECT * FROM pesme WHERE id=". $id;
-        $rezultat= mysqli_query($conn, $sql);
-        $red=mysqli_fetch_assoc($rezultat);
-        $broj_lajkova=$red["lajkova"];
-        $novi_broj_lajkova=$broj_lajkova+1;
-        $sql = "SELECT * FROM pesme WHERE id=". $id;
+if(isset($_SESSION["id"])){
+    require "vezasabazom.php";
+    $id_pesme = $_POST["id"];
+    $id_korisnika= $_POST["profil"];
+    if($id_korisnika==$_SESSION["id"]){
+        $sql = "SELECT lajkovao FROM pesme WHERE id=". $id_pesme;
         $rezultat= mysqli_query($conn, $sql);
         $red=mysqli_fetch_assoc($rezultat);
         $lajkovao=$red["lajkovao"];
-        if($lajkovao=="null"|| $lajkovao==null || $lajkovao==""){
-            $niz = array($_SESSION["ime"]);
-            $jsonniz= json_encode($niz);
-            $sql = "UPDATE pesme set lajkova=?, lajkovao=? WHERE id=?";
-            $stmt= mysqli_stmt_init($conn); 
-            if(!mysqli_stmt_prepare($stmt, $sql)){
-                echo "greška 1";
-                exit();
-            }
-            else{
-                mysqli_stmt_bind_param($stmt, "sss", $novi_broj_lajkova, $jsonniz,  $id);
-                mysqli_stmt_execute($stmt);
-                $sql = "SELECT * FROM pesme WHERE id=". $id;
-                $rezultat= mysqli_query($conn, $sql);
-                $red=mysqli_fetch_assoc($rezultat);
-                $broj_lajkova=$red["lajkova"];
-                echo $broj_lajkova;
-            }
+        if(gettype($lajkovao)=="NULL" or $lajkovao==="[]"){
+            $niz = array($_SESSION["id"]);
+            ubaci_u_bazu();
+            vrati();
         }
         else{
-            $niz=json_decode($lajkovao);
-            if(in_array($_SESSION["ime"], $niz)){
-                $novi_broj_lajkova=$broj_lajkova -1;
-                $index = array_search($_SESSION["ime"],$niz);
+            $niz=json_decode($lajkovao,true);
+            if(in_array($_SESSION["id"], $niz)){
+                $index = array_search($_SESSION["id"],$niz);
                 if($index !== FALSE){
                     unset($niz[$index]);
+                    $niz = array_values($niz);
                 }
-                $jsonniz=json_encode($niz);
-                $sql = "UPDATE pesme set lajkova=?, lajkovao=? WHERE id=?";
-                $stmt= mysqli_stmt_init($conn); 
-                if(!mysqli_stmt_prepare($stmt, $sql)){
-                    echo "greška 1";
-                    exit();
-                }
-                else{
-                    mysqli_stmt_bind_param($stmt, "sss", $novi_broj_lajkova, $jsonniz,  $id);
-                    mysqli_stmt_execute($stmt);
-                    $sql = "SELECT * FROM pesme WHERE id=". $id;
-                    $rezultat= mysqli_query($conn, $sql);
-                    $red=mysqli_fetch_assoc($rezultat);
-                    $broj_lajkova=$red["lajkova"];
-                    echo $broj_lajkova;
-                }  
+                ubaci_u_bazu();
+                vrati();
             }
             else{
-                array_push($niz, $_SESSION["ime"]);
-                $jsonniz=json_encode($niz);
-                $sql = "UPDATE pesme set lajkova=?, lajkovao=? WHERE id=?";
-                $stmt= mysqli_stmt_init($conn); 
-                if(!mysqli_stmt_prepare($stmt, $sql)){
-                    echo "greška 1";
-                    exit();
-                }
-                else{
-                    mysqli_stmt_bind_param($stmt, "sss", $novi_broj_lajkova, $jsonniz,  $id);
-                    mysqli_stmt_execute($stmt);
-                    $sql = "SELECT * FROM pesme WHERE id=". $id;
-                    $rezultat= mysqli_query($conn, $sql);
-                    $red=mysqli_fetch_assoc($rezultat);
-                    $broj_lajkova=$red["lajkova"];
-                    echo $broj_lajkova;
-                }
+                array_push($niz, $_SESSION["id"]);
+                ubaci_u_bazu();
+                vrati();
             }
         }
     }
 }
-else{
-    echo "Ulogujte se!";
+function ubaci_u_bazu(){
+    global $conn, $niz, $id_pesme;
+    $jsonniz= json_encode($niz);
+    $sql = "UPDATE pesme set  lajkovao=? WHERE id=?";
+    $stmt= mysqli_stmt_init($conn); 
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        echo "greška 1";
+        exit();
+    }
+    else{
+        mysqli_stmt_bind_param($stmt, "ss",  $jsonniz,  $id_pesme);
+        mysqli_stmt_execute($stmt);
+    }
+}
+function vrati(){
+    global $conn, $id_pesme;
+    $sql = "SELECT lajkovao FROM pesme WHERE id=". $id_pesme;
+    $rezultat= mysqli_query($conn, $sql);
+    $red=mysqli_fetch_assoc($rezultat);
+    $lajkovao=$red["lajkovao"];
+    $tip=gettype($lajkovao);
+    if($tip==="NULL" || $lajkovao==="[]"){
+        $broj_lajkova=0;
+    }
+    else{
+        $niz_lajkova=json_decode($lajkovao);
+        $broj_lajkova=count($niz_lajkova);
+    }
+    echo $broj_lajkova;
 }
 ?>
--- objava/ispis.php
+++ objava/ispis.php
@@ -2,32 +2,41 @@
 $rezultat= mysqli_query($conn, $sql);
 $brojrezulta=mysqli_num_rows($rezultat);
 while($red=mysqli_fetch_assoc($rezultat)){
-require "vreme.php";
-    $sql =  "SELECT * FROM korisnici WHERE ime='". $red["pisac"] ."';";
-    $rezultatprofilna= mysqli_query($conn, $sql);
-    $brojrezulta=mysqli_num_rows($rezultatprofilna);
-    while ($redprofilna = mysqli_fetch_assoc($rezultatprofilna)){
-            $tip=gettype($redprofilna["profilna"]);
-            if($tip=="null" || $tip=="NULL" || $tip==null){
-              $profilna="fajlovi/profile-icon.svg";
-            }
-            else if ($tip == "string") {
-              $profilna = $redprofilna["profilna"];
-            }
-          }
+	$lajkovao= $red["lajkovao"];
+	$tip_lajkovao= gettype($lajkovao);
+	if($tip_lajkovao=="NULL"){
+		$broj_lajkova=0;
+	}
+	else{
+		$niz_lajkova=json_decode($lajkovao);
+		$broj_lajkova=count($niz_lajkova);
+	}
+	require "vreme.php";
+	$sql_za_pisca =  "SELECT * FROM korisnici WHERE id='". $red["id_pisca"] ."';";
+	$rezultati_za_pisca= mysqli_query($conn, $sql_za_pisca);
+	while ($red_za_pisca = mysqli_fetch_assoc($rezultati_za_pisca)){
+		$korisnicko_ime_pisca= $red_za_pisca["username"];
+		$profilna_pisca=$red_za_pisca["profilna"];
+		$tip=gettype($profilna_pisca);
+		if($tip=="null" || $tip=="NULL" || $tip==null){
+			$profilna="fajlovi/profile-icon.svg";
+		}
+		else if ($tip == "string") {
+			$profilna_pisca = "fajlovi/". $profilna_pisca;
+		}
+	}
 echo '<div class="card text-center kartica '.$red["boja"].'" id="'.$red["id"].'">';
 echo    '<div class="card-header pisac">';
 echo        '<div class="profilna-pisca">
-                <a href="profil.php?profil='.$red["pisac"].'">
-                  	<img src="'.$profilna.'">
+                <a href="proflili/'.$korisnicko_ime_pisca.'">
+                  	<img src="'.$profilna_pisca.'">
                 </a>
             </div>';
-echo        '<a class="link-prema-piscu" href="profil.php?profil='.$red["pisac"].'">'. $red["pisac"] . '</a>';
+echo        '<a class="link-prema-piscu" href="proflili/'.$korisnicko_ime_pisca.'">'. $korisnicko_ime_pisca . '</a>';
 echo        '<div class="tacke" onclick="prikaziOpcije(this)">
                 <img src="fajlovi/tacke.png">
                 <div class="opcije">';
-
-                if($_SESSION["ovlascenje"]=="admin" || $_SESSION["ime"]==$red["pisac"] ){
+                if($_SESSION["ovlascenje"]=="admin" || $_SESSION["id"]==$red["id_pisca"]){
                 echo'<a onclick="obrisi('.$red["id"].')" class="opcija btn btn-danger">Obriši</a>';
                 }
                 echo '
@@ -43,37 +52,35 @@ echo '<div class="komentari">neki-komentar</div>';
 echo '<button onclick="prikazivise('.$red["id"].')" class="btn btn-primary prikazi-vise">Procitaj vise</a>';
 echo '</div>
 <div class="card-footer text-muted vreme">';
-if($ovlascenje=="nelogovan"){
+if($ovlascenje=="neprijavljen"){
   echo '<div onclick="lajkujnelogovan()"  class="srce">
           <img src="fajlovi/srce-prazno.svg">
-            <span id="' .$red["id"] . 'brojlajkova">' . $red["lajkova"] .'</span>
+            <span id="' .$red["id"] . 'brojlajkova">' . $broj_lajkova .'</span>
         </div>
         <div class="vreme-tekst">' . $vreme . '</div>
         <div onclick="prikazikomentare('.$red["id"].')" class="ikonicazakomentarkontejner">
           <img class="ikonicazakomentar" src="fajlovi/komentar.svg" /> </div>';
 }
 else{
-  $lajkovao=$red["lajkovao"];
-  $niz=json_decode($lajkovao);
-  if(in_array($_SESSION["ime"], $niz)){
-    echo '<div onclick="lajkuj('.$red["id"].","."'".$_SESSION["ime"]."')".'"'. ' class="srce">
+  if($broj_lajkova>0 and in_array($_SESSION["id"], $niz_lajkova)){
+    echo '<div onclick="lajkuj('.$red["id"].","."'".$_SESSION["id"]."')".'"'. ' class="srce">
             <img src="fajlovi/srce-puno.png">
-            <span id="' .$red["id"] . 'brojlajkova">' . $red["lajkova"] .'</span>
+            <span id="' .$red["id"] . 'brojlajkova">' . $broj_lajkova .'</span>
           </div>
           <div class="vreme-tekst">' . $vreme . '</div>
           <div onclick="prikazikomentare('.$red["id"].')" class="ikonicazakomentarkontejner">
           <img class="ikonicazakomentar" src="fajlovi/komentar.svg" /> </div>';
   }
   else{
-    echo '<div onclick="lajkuj('.$red["id"].","."'".$_SESSION["ime"]."')".'"'. ' class="srce">
+    echo '<div onclick="lajkuj('.$red["id"].","."'".$_SESSION["id"]."')".'"'. ' class="srce">
             <img src="fajlovi/srce-prazno.svg">
-            <span id="' .$red["id"] . 'brojlajkova">' . $red["lajkova"] .'</span>
+            <span id="' .$red["id"] . 'brojlajkova">' . $broj_lajkova .'</span>
           </div>
           <div class="vreme-tekst">' . $vreme .'</div>
           <div onclick="prikazikomentare('.$red["id"].')" class="ikonicazakomentarkontejner">
             <img class="ikonicazakomentar" src="fajlovi/komentar.svg" />
           </div>';
-  }
-}
+    	}
+	}
 echo '</div></div>';
 }
