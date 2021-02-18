d15e2600460dfb2291d642b50909228fe75ec5f0
anonymousvegan <majjunjulavgust@gmail.com>
2021-02-19 00:54:02 +0100

    brisanje dodato!

--- backend/obrisi.php
+++ backend/obrisi.php
@@ -6,40 +6,30 @@ if(isset($_SESSION["id"])){
     $idkorisnika=$_SESSION["id"];
     $sql = "SELECT * FROM korisnici where id=". $idkorisnika;
     $rezultat= mysqli_query($conn, $sql);
-    $brojrezulta=mysqli_num_rows($rezultat);
-    while($red=mysqli_fetch_assoc($rezultat)){
-        $korisnickoime = $red["ime"];
-        if($red["ovlascenje"]=="admin"){
-            $sql = "DELETE FROM pesme WHERE id=?;";
-            $stmt=mysqli_stmt_init($conn);
-            if(!mysqli_stmt_prepare($stmt, $sql)){
-                echo "greška u brisanju iz baze 1";
-                exit();
-            }
-            else{
-                mysqli_stmt_bind_param($stmt, "s", $idpesme);
-                mysqli_stmt_execute($stmt);
-            }
+    $red=mysqli_fetch_assoc($rezultat);
+    if($red["ovlascenje"]=="admin"){
+        obrisi_iz_baze();
+    }
+    else{
+        $sql = "SELECT * FROM pesme where id=".  $idpesme;
+        $rezultat= mysqli_query($conn, $sql);
+        $red=mysqli_fetch_assoc($rezultat);
+        $pisac= $red["id_pisca"];
+        if($pisac==$_SESSION["id"]){
+            obrisi_iz_baze();
         }
-        else{
-            $sql = "SELECT * FROM pesme where id=".  $idpesme;
-            $rezultat= mysqli_query($conn, $sql);
-            $brojrezulta=mysqli_num_rows($rezultat);
-            while($red=mysqli_fetch_assoc($rezultat)){
-                $pisac= $red["pisac"];
-                if($pisac==$korisnickoime && $pisac==$_SESSION["ime"]){
-                    $sql = "DELETE FROM pesme WHERE id=?;";
-                    $stmt=mysqli_stmt_init($conn);
-                    if(!mysqli_stmt_prepare($stmt, $sql)){
-                        echo "greška u brisanju iz baze2";
-                        exit();
-                    }
-                    else{
-                        mysqli_stmt_bind_param($stmt, "s", $idpesme);
-                        mysqli_stmt_execute($stmt);
-                    }
-                }
-            }
-        }
+    }
+}
+function obrisi_iz_baze(){
+    global $conn, $idpesme;
+    $sql = "DELETE FROM pesme WHERE id=?;";
+    $stmt=mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        echo "greška u brisanju iz baze ";
+        exit();
+    }
+    else{
+        mysqli_stmt_bind_param($stmt, "s", $idpesme);
+        mysqli_stmt_execute($stmt);
     }
 }
